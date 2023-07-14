3450db6361034947f0c2c9876db8cb002a73a4a6
Micah McGathey <micahkicks@yahoo.com>
2023-07-14 16:41:53 -0500

    Updates

---
+++ precache-manifest.f70140e4f575bfa2e68e9572842f3c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a82aca0c0f45a6867048723c5c1b6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "807a4ef1c3c20b6c2f54",
+    "url": "/static/css/main.750ee5bf.chunk.css"
+  },
+  {
+    "revision": "79da71e694ad10009844",
+    "url": "/static/js/2.2feb1bb1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2feb1bb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807a4ef1c3c20b6c2f54",
+    "url": "/static/js/main.51bbc9f8.chunk.js"
+  },
+  {
+    "revision": "712a493cb6bbaa95b925",
+    "url": "/static/js/runtime-main.cef06927.js"
+  }
+]);
